62baaa8260ff3d27b2a34bc5f845a1ae9869d9e0
anton <anton.kruten@gmail.com>
2022-01-28 15:09:52 +0300

    init commit

---
+++ src/main/java/com/kruten/service_with_search/ServiceWithSearchApplication.java
@@ -0,0 +1,14 @@
+package com.kruten.service_with_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class ServiceWithSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceWithSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kruten/service_with_search/ServiceWithSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruten.service_with_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWithSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
