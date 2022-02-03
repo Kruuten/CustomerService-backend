eb15b4cf2afc62e779f1505dcbba377e078ec3b2
anton <anton.kreutn@gmail.com>
2022-02-03 21:41:32 +0300

    added main view of customers

--- frontend/customer-service-frontend/src/App.js
+++ frontend/customer-service-frontend/src/App.js
@@ -1,18 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ListCustomerComponent from './components/ListCustomerComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import CreateCustomerComponent from './components/CreateCustomerComponent';
+import TestComponent from './components/TestComponent';
 
 function App() {
   return (
     <div>
       <HeaderComponent />
-    
-        <div className="container">
-          <ListCustomerComponent />
+      <Router>
+        <div className='container'>
+            <div className="container">
+              <Routes>
+                <Route path = '/' exact element = {<ListCustomerComponent />}></Route>
+                <Route path = '/customers'  element = {<ListCustomerComponent />}></Route>
+                <Route path = '/add-customer'  element = {<CreateCustomerComponent />}></Route>
+                <Route path = '/test'  element = {<TestComponent />}></Route>
+              </Routes>
+            </div>
         </div>
-        <FooterComponent />
+      </Router>
+      <FooterComponent /> 
     </div>
   );
 }
--- frontend/customer-service-frontend/src/components/ListCustomerComponent.jsx
+++ frontend/customer-service-frontend/src/components/ListCustomerComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CustomerService from '../services/CustomerService';
 
+
 class ListCustomerComponent extends Component {
     constructor(props){
         super(props)
@@ -9,18 +10,27 @@ class ListCustomerComponent extends Component {
             customers: []
 
         }
+        this.addCustomer = this.addCustomer.bind(this);
     }
 
     componentDidMount(){
         CustomerService.getCustomers().then((res) =>{
             this.setState({customers: res.data});
         });
+
+    }
+
+    addCustomer(){
+        this.customer.history.push('/add-customer');
     }
 
     render() {
         return (
             <div>
               <h2 className='text-center'>Customer List</h2> 
+              <div className='row'>
+                  <button className='btn-primary' onClick={() => this.addCustomer()}>Add</button>
+              </div>
               <div className='row'>
                     <table className='table table-striped table-bordered'>
 
@@ -45,8 +55,22 @@ class ListCustomerComponent extends Component {
                                         <td>{customer.lastName}</td>
                                         <td>{customer.middleName}</td>
                                         <td>{customer.sex}</td>
-                                        <td>{customer.regAddress}</td>
-                                        <td>{customer.actAddress}</td>
+                                        <td className='breaking-line'>
+                                          {'Country: ' + customer.registredAddress.country + '\n'
+                                         + 'Region: ' + customer.registredAddress.region + '\n'
+                                         + 'City: ' + customer.registredAddress.city + '\n'
+                                         + 'Street: ' + customer.registredAddress.street + '\n'
+                                         + 'House: ' + customer.registredAddress.house + '\n'
+                                         + 'Flat: ' + customer.registredAddress.flat + '\n'}
+                                        </td> 
+                                        <td className='breaking-line'>
+                                          {'Country: ' + customer.actualAddress.country + '\n'
+                                         + 'Region: ' + customer.actualAddress.region + '\n' 
+                                         + 'City: ' + customer.actualAddress.city + '\n' 
+                                         + 'Street: ' + customer.actualAddress.street + '\n' 
+                                         + 'House: ' + customer.actualAddress.house + '\n' 
+                                         + 'Flat ' + customer.actualAddress.flat + '\n'}
+                                        </td>
                                     </tr>
                                 )
                             }
