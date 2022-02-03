f77740400b68a09dfa8d5a858ad5bddf19495b85
anton <anton.kruten@gmail.com>
2022-02-03 22:27:00 +0300

    deleted necessary imports

--- src/test/java/com/kruten/service_with_search/ServiceWithSearchApplicationTests.java
+++ src/test/java/com/kruten/service_with_search/ServiceWithSearchApplicationTests.java
@@ -1,9 +1,10 @@
 package com.kruten.service_with_search;
 
+import com.kruten.service_with_search.serviceTest.CustomerServiceTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CustomerServiceTest.class)
 class ServiceWithSearchApplicationTests {
 
     @Test
