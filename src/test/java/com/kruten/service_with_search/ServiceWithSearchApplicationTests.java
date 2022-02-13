040c2a184c1b06e629cc67cca41c39f3f503356f
anton <anton.kruten@gmail.com>
2022-02-13 22:09:27 +0300

    added unit tests

--- src/test/java/com/kruten/service_with_search/ServiceWithSearchApplicationTests.java
+++ src/test/java/com/kruten/service_with_search/ServiceWithSearchApplicationTests.java
@@ -1,10 +1,10 @@
 package com.kruten.service_with_search;
 
-import com.kruten.service_with_search.serviceTest.CustomerServiceTest;
+import com.kruten.service_with_search.contollerTest.CustomServiceControllerTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = CustomerServiceTest.class)
+@SpringBootTest(classes = CustomServiceControllerTest.class)
 class ServiceWithSearchApplicationTests {
 
     @Test
