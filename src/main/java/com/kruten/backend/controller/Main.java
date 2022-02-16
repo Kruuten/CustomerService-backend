ee0eb8ece5b9343fe35856d42e9789763d8a2d22
anton <anton.kruten@gmail.com>
2022-02-16 13:48:48 +0300

    Changed CustomerServiceController class name to Main

--- src/main/java/com/kruten/backend/controller/Main.java
+++ src/main/java/com/kruten/backend/controller/Main.java
@@ -14,7 +14,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/")
-public class CustomServiceController {
+public class Main {
 
     @Autowired
     private CustomerService customerService;
